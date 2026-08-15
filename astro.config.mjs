29d912003e9762d1cc1ee6cb19b4bd5b5fbcd542
camy0414 <128252168+camy0414@users.noreply.github.com>
2026-08-15 15:04:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://github.io',
-  base: '/PovedaCamyla.github.io',
+  base: '/PovedaCamyla.github.io/',
   trailingSlash: "ignore",
   prefetch: {
     prefetchAll: true
